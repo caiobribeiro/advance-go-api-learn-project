caa5c0f33d36e9ae8a72a93f79157c44f38e771b
Caio Beraldi Ribeiro <caio_ribeiro@protonmail.com>
2025-10-05 12:34:06 -0300

    feat: implementation of core handler

--- internal/handlers/core_handler.go
+++ internal/handlers/core_handler.go
@@ -1,10 +1,21 @@
 package handlers
 
+import (
+	"database/sql"
+
+	"github.com/caiobribeiro/advance-go-api-learn-project/internal/store"
+)
+
 type Handler struct {
 	//  DB instance
+	DB *sql.DB
 	// Query stores
+	Queries *store.Queries
 }
 
-func NewHandlers() *Handler {
-	return &Handler{}
+func NewHandlers(db *sql.DB, queries *store.Queries) *Handler {
+	return &Handler{
+		DB:      db,
+		Queries: queries,
+	}
 }
